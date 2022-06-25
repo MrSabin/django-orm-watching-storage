0d0746563b3df316b8615951e13a38b78bec9bf0
MrSabin <sabin.21011986@gmail.com>
2022-06-25 17:30:16 +0300

    Changed site view to real information
    Added get_duration() function
    Added format_duration() function

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,33 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+from django.utils import timezone
+
+
+def get_duration(time):
+    duration = timezone.localtime() - timezone.localtime(time)
+    return format_duration(duration)
+
+
+def format_duration(duration):
+    hours = duration.days * 24 + duration.seconds // 3600
+    minutes = (duration.seconds % 3600) // 60
+    seconds = (duration.seconds % 60)
+    return f"{hours}:{minutes}:{seconds}"
 
 
 def storage_information_view(request):
     # Программируем здесь
-
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
+    opened_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
+    for visit in opened_visits:
+        time_passed = get_duration(visit.entered_at)
+        time_entered = timezone.localtime(visit.entered_at)
+        visit_details = {
+            'who_entered': f'{visit.passcard}',
+            'entered_at': f'{time_entered}',
+            'duration': f'{time_passed}'}
+        non_closed_visits.append(visit_details)
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
