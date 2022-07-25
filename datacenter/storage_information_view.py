096830d29a05c7d1656ed468f0d639de394c1456
MrSabin <sabin.21011986@gmail.com>
2022-07-25 22:20:49 +0300

    Changed variable name

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,5 +1,5 @@
 from datacenter.models import Visit, is_visit_long
-from datacenter.time_processing import get_duration, format_duration
+from datacenter.time_processing import format_duration, get_duration
 from django.shortcuts import render
 from django.utils import timezone
 
@@ -10,12 +10,12 @@ def storage_information_view(request):
     for visit in opened_visits:
         time_passed = get_duration(visit.entered_at)
         time_entered = timezone.localtime(visit.entered_at)
-        flag = is_visit_long(visit)
+        is_strange = is_visit_long(visit)
         visit_details = {
             'who_entered': f'{visit.passcard}',
             'entered_at': f'{time_entered}',
             'duration': f'{time_passed}',
-            'flag': f'{flag}'}
+            'is_strange': f'{is_strange}'}
         serialized_visits.append(visit_details)
     context = {
         'non_closed_visits': serialized_visits,
